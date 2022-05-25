c185d894eb65a50bbb4149592da6826f43f96809
evilditch <ronja@pahaoja.fi>
2022-05-25 18:59:44 +0300

    tehtävä 1.2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -4,11 +4,19 @@ const Header = (props) => {
     )
 }
 
+const Part = (props) => {
+    return (
+        <p>{props.part} {props.exersices} </p>
+    )
+}
+
 const Content = (props) => {
     return(
-      <p>
-        {props.part} {props.exercises}
-      </p>
+        <div>
+        <Part part={props.part1} exersices={props.exercises1} />
+        <Part part={props.part2} exersices={props.exercises2} />
+        <Part part={props.part3} exersices={props.exercises3} />
+        </div>
     )
 }
 
@@ -30,9 +38,9 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} 
+      part2={part2} exercises2={exercises2} 
+      part3={part3} exercises3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
