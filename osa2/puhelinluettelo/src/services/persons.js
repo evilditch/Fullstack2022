01decf09cae779d523284e65eb7eb9f5d42c23a1
evilditch <ronja@pahaoja.fi>
2022-06-10 18:53:18 +0300

    tehtävä 3.9

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl)
@@ -12,6 +12,5 @@ const create = (newObj) => {
 const deletePerson = (id) => {
   axios.delete(`${baseUrl}/${id}`)
 }
-
 
 export default { getAll, create, deletePerson }
