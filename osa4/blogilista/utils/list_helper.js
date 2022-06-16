754e5baddac3b5669b2431e403261eaf30c27013
evilditch <ronja@pahaoja.fi>
2022-06-16 12:22:33 +0300

    tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sum = blogs.reduce((value, blog) => {
+    return value + blog.likes
+  }, 0)
+  console.log(sum)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
