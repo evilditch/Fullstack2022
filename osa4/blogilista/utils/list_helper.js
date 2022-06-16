54cb2f5edba7d122cd3c0f26fafdf326e952d26b
evilditch <ronja@pahaoja.fi>
2022-06-16 15:36:38 +0300

    tehtävä 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,11 +6,19 @@ const totalLikes = (blogs) => {
   const sum = blogs.reduce((value, blog) => {
     return value + blog.likes
   }, 0)
-  console.log(sum)
   return sum
+}
+
+const favoriteBlogs = (blogs) => {
+  const favorite = blogs.reduce((prev, current) => {
+    // console.log(prev)
+    return prev.likes > current.likes ? prev : current
+  }, { title: '', author: '', likes: 0 })
+  return { title: favorite.title, author: favorite.author, likes: favorite.likes }
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlogs,
 }
