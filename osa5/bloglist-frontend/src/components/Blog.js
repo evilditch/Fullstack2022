3eb14975b527aa07319fd7f1dbc86f7d6cfe0b91
evilditch <ronja@pahaoja.fi>
2022-07-01 15:32:32 +0300

    tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,28 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [expanded, setExpanded] = useState(false)
+  
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
+  const expand = (event) => {
+    setExpanded(!expanded)
+  }
+
+  return (
+    <div>
+      <button onClick={expand} aria-expanded={expanded}>{blog.title} {blog.author} { expanded ? '-' : '+' }</button>
+      <div style={showWhenExpanded}>
+        <dl>
+          <dt>Url</dt><dd>{blog.url}</dd>
+          <dt>Likes</dt><dd>{blog.likes}</dd>
+      <dt>User</dt><dd>{blog.user.username}</dd>
+        </dl>
+        <button>Like</button>
+      <button onClick={expand}>Hide</button>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
